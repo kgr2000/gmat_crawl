0d9ae7b9e2c73b64d1d12d10cd6d17bc2f7785d9
Garmgoon <garmgoon@gmail.com>
2020-03-02 03:03:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function main() {
 
   await page.goto(
 <<<<<<< HEAD
+<<<<<<< HEAD
     `https://gmatclub.com/forum/search.php?st=0&sk=t&sd=d&sr=topics&search_id=search_tag&search_id=search_tag&selected_search_tags%5B%5D=1348&selected_search_tags%5B%5D=1281&selected_search_tags%5B%5D=1282&selected_search_tags%5B%5D=1283&selected_search_tags%5B%5D=1284&selected_search_tags%5B%5D=1285&selected_search_tags%5B%5D=1286&selected_search_tags%5B%5D=1287&selected_search_tags%5B%5D=1288&selected_search_tags%5B%5D=1330&selected_search_tags%5B%5D=1335&search_tags=exact&terms=both&sj=one&start=0`);
 
   await page.waitFor(".topicsName.rc");
@@ -38,6 +39,12 @@ async function main() {
   );
   await page.waitFor("td.topicsName.rc");
 
+=======
+    "https://gmatclub.com/forum/search.php?selected_search_tags%5B%5D=164&t=0&search_tags=exact&submit=Search"
+  );
+  await page.waitFor("td.topicsName.rc");
+
+>>>>>>> parent of 748b7c4... from laptop
   let ehList = await page.$$("td.topicsName.rc");
   for (let eh of ehList) {
     let title = await eh.$eval("span.topicTitle", function(el) {
@@ -46,6 +53,9 @@ async function main() {
     console.log(title);
   }
   //   broswer.close();
+<<<<<<< HEAD
+>>>>>>> parent of 748b7c4... from laptop
+=======
 >>>>>>> parent of 748b7c4... from laptop
 }
 
