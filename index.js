748b7c4740dc8169fae2aa3a0bc0d4086c0ba762
kgr2000 <garmgoon@gmail.com>
2020-03-02 02:52:23 +0900

    from laptop

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 let puppeteer = require("puppeteer");
 
 async function main() {
-  let broswer = await puppeteer.launch({ headless: false });
+  let broswer = await puppeteer.launch({ headless: true });
   let page = await broswer.newPage();
   await page.goto(
-    "https://gmatclub.com/forum/search.php?selected_search_tags%5B%5D=164&t=0&search_tags=exact&submit=Search"
+    "https://gmatclub.com/forum/search.php?st=0&sk=t&sd=d&sr=topics&search_id=search_tag&search_id=search_tag&selected_search_tags%5B%5D=164&search_tags=exact&terms=both&sj=one&start=50"
   );
-  await page.waitFor("td.topicsName.rc");
+  await page.waitFor(".topicsName.rc");
 
-  let ehList = await page.$$("td.topicsName.rc");
-  for (let eh of ehList) {
-    let title = await eh.$eval("span.topicTitle", function(el) {
-      return el.innerText;
-    });
-    console.log(title);
-  }
-  //   broswer.close();
+  let links = await page.$$eval(".topicsName.rc", options => {
+    return options.map(option => option.innerHTML);
+  });
+  console.log(links);
+  // let ehList = await page.$$("td.topicsName.rc");
+  // for (let eh of ehList) {
+  //   let title = await eh.$eval("span.topicTitle", function(el) {
+  //     return el.innerText;
+  //   });
+  //   console.log(title);
+  // }
+  broswer.close();
 }
 
 main();
